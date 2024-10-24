abcb8e2c8d0f6b38bbe99d3c6f07b1b1869abb86
Ritesh Bhalerao <105015931+Riteshbhalerao11@users.noreply.github.com>
2024-10-24 15:03:05 +0530

    Update niche_tuner.sh

--- Tuners/Tuner_vanilla/niche_tuner.sh
+++ Tuners/Tuner_vanilla/niche_tuner.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-#SBATCH -A m4392
-#SBATCH -C gpu&hbm80g
-#SBATCH -q regular
-#SBATCH -t 24:00:00
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-task=4
-#SBATCH --cpus-per-task=128
-#SBATCH --output="/pscratch/sd/r/ritesh11/ew_logs/tuner/slurm-%j.out"
-#SBATCH --error="/pscratch/sd/r/ritesh11/ew_logs/tuner/slurm-%j.out"
-#SBATCH --mail-user=ritesh.slurm@gmail.com
-#SBATCH --mail-type=ALL
+#SBATCH ... args
 
 
-module load pytorch/2.1.0-cu12
+module load pytorch/X
 
 nvidia-smi
 
